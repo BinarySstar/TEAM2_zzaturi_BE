c14205d210e5c78b34bf0c5dc2415dbef504570b
Jinseong <hdss8234@gmail.com>
2024-09-28 23:55:22 +0900

    fix : API 명세화

--- src/main/java/goorm/zzaturi/domain/board/controller/BoardController.java
+++ src/main/java/goorm/zzaturi/domain/board/controller/BoardController.java
@@ -5,6 +5,14 @@ import goorm.zzaturi.domain.board.dto.request.BoardUpdateRequestDto;
 import goorm.zzaturi.domain.board.entity.Board;
 import goorm.zzaturi.domain.board.entity.Category;
 import goorm.zzaturi.domain.board.service.BoardService;
+import goorm.zzaturi.global.exception.BoardNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,34 +22,57 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/board")
+@Tag(name = "Board Controller", description = "게시판 API")
 public class BoardController {
 
     private final BoardService boardService;
 
+    @Operation(summary = "게시판 목록 조회", description = "카테고리 별로 게시판 목록을 조회합니다.")
+    @Parameter(name = "category", description = "카테고리")
+    @ApiResponse(responseCode = "200", description = "게시판 목록 조회 성공", content=@Content(schema=@Schema(implementation=Board.class)))
     @GetMapping("/list")
     public ResponseEntity<List<Board>> getBoardList(@RequestParam Category category) {
         List<Board> boardList = boardService.findAllByCategory(category);
         return ResponseEntity.ok(boardList);
     }
 
+    @Operation(summary = "게시글 상세 조회", description = "게시글을 상세하게 조회합니다.")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "게시글 상세 조회 성공", content = @Content(schema = @Schema(implementation = Board.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글", content = @Content(schema = @Schema(implementation = BoardNotFoundException.class)))
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Board> get(@PathVariable Long id) {
         Board board = boardService.findById(id);
         return ResponseEntity.ok(board);
     }
 
+    @Operation(summary = "게시글 작성", description = "게시글을 작성합니다.")
+    @Parameter(name = "BoardCreateRequestDto", description = "게시글 작성 요청 데이터")
+    @ApiResponse(responseCode = "200", description = "게시글 작성 성공", content=@Content(schema=@Schema(implementation=Board.class)))
     @PostMapping
     public ResponseEntity<Board> create(@RequestBody BoardCreateRequestDto requestDto) {
         Board board = boardService.create(requestDto);
         return ResponseEntity.ok(board);
     }
 
+    @Operation(summary = "게시글 수정", description = "게시글을 수정합니다.")
+    @Parameter(name = "BoardUpdateRequestDto", description = "게시글 수정 요청 데이터")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "게시글 수정 성공", content = @Content(schema = @Schema(implementation = Board.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글", content = @Content(schema = @Schema(implementation = BoardNotFoundException.class)))
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Board> update(@PathVariable Long id, @RequestBody BoardUpdateRequestDto requestDto) {
         Board board = boardService.update(id, requestDto);
         return ResponseEntity.ok(board);
     }
 
+    @Operation(summary = "게시글 삭제", description = "게시글을 삭제합니다.")
+    @ApiResponses(value={
+            @ApiResponse(responseCode = "200", description = "게시글 삭제 성공", content = @Content(schema = @Schema(implementation = Board.class))),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글", content = @Content(schema = @Schema(implementation = BoardNotFoundException.class)))
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Board> delete(@PathVariable Long id) {
         Board board = boardService.delete(id);
--- src/main/java/goorm/zzaturi/domain/comment/controller/CommentController.java
+++ src/main/java/goorm/zzaturi/domain/comment/controller/CommentController.java
@@ -5,6 +5,15 @@ import goorm.zzaturi.domain.comment.dto.request.CommentUpdateRequestDto;
 import goorm.zzaturi.domain.comment.dto.response.CommentCreateResponseDto;
 import goorm.zzaturi.domain.comment.entity.Comment;
 import goorm.zzaturi.domain.comment.service.CommentService;
+import goorm.zzaturi.global.exception.BoardNotFoundException;
+import goorm.zzaturi.global.exception.CommentNotFoundException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +23,38 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
+@Tag(name = "Comment Controller", description = "댓글 API")
 public class CommentController {
 
     private final CommentService commentService;
 
+    @Operation(summary = "댓글 목록 조회", description = "게시판에 포함되어 있는 모든 댓글을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "댓글 목록 조회 성공", content=@Content(schema=@Schema(implementation=Comment.class)))
     @GetMapping
     public ResponseEntity<List<Comment>> getAllComments(@RequestParam Long boardId) {
         List<Comment> comments = commentService.getAllComments(boardId);
         return ResponseEntity.ok(comments);
     }
 
-    @PostMapping()
+    @Operation(summary = "댓글 작성", description = "게시판에 댓글을 작성합니다.")
+    @Parameter(name = "CommentCreateRequestDto", description = "댓글 작성 요청 데이터")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "댓글 작성 성공", content = @Content(schema = @Schema(implementation = CommentCreateResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 게시판", content = @Content(schema = @Schema(implementation = BoardNotFoundException.class)))
+    })
+    @PostMapping
     public ResponseEntity<CommentCreateResponseDto> createComment(@RequestParam Long boardId,
                                                                   @RequestBody CommentCreateRequestDto requestDto) {
         Comment comment = commentService.createComment(boardId, requestDto);
         return ResponseEntity.ok(CommentCreateResponseDto.of(comment));
     }
 
+    @Operation(summary = "댓글 수정", description = "게시판에 작성된 댓글을 수정합니다.")
+    @Parameter(name = "CommentUpdateRequestDto", description = "댓글 수정 요청 데이터")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "댓글 수정 성공", content = @Content(schema = @Schema(implementation = CommentCreateResponseDto.class))),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 댓글", content = @Content(schema = @Schema(implementation = CommentNotFoundException.class)))
+    })
     @PutMapping("/{commentId}")
     public ResponseEntity<CommentCreateResponseDto> updateComment(@PathVariable Long commentId,
                                                                   @RequestBody CommentUpdateRequestDto requestDto) {
@@ -38,6 +62,11 @@ public class CommentController {
         return ResponseEntity.ok(CommentCreateResponseDto.of(comment));
     }
 
+    @Operation(summary = "댓글 삭제", description = "게시판에 작성된 댓글을 삭제합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "댓글 삭제 성공"),
+        @ApiResponse(responseCode = "404", description = "존재하지 않는 댓글", content = @Content(schema = @Schema(implementation = CommentNotFoundException.class)))
+    })
     @DeleteMapping("/{commentId}")
     public ResponseEntity<Void> deleteComment(@PathVariable Long commentId) {
         commentService.deleteComment(commentId);
