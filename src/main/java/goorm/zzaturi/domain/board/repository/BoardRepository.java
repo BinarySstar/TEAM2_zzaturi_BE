0ede23cb2d8501f6cc6af1e6a68cb054990fa510
Jinseong <hdss8234@gmail.com>
2024-09-28 20:30:13 +0900

    feat : 카테고리 별 모든 게시글 조회

--- src/main/java/goorm/zzaturi/domain/board/repository/BoardRepository.java
+++ src/main/java/goorm/zzaturi/domain/board/repository/BoardRepository.java
@@ -1,9 +1,14 @@
 package goorm.zzaturi.domain.board.repository;
 
 import goorm.zzaturi.domain.board.entity.Board;
+import goorm.zzaturi.domain.board.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
+
+    List<Board> findAllByCategory(Category category);
 }
