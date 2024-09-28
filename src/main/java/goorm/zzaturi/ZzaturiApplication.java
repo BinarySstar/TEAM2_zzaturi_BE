cf5258490aff2102b945c2345f91a563d15465d3
jjh75607 <dkrak5738@naver.com>
2024-09-28 18:20:22 +0900

    init

---
+++ src/main/java/goorm/zzaturi/ZzaturiApplication.java
@@ -0,0 +1,13 @@
+package goorm.zzaturi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZzaturiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZzaturiApplication.class, args);
+    }
+
+}
